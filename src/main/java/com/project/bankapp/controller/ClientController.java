e6b78e41f296fa969e621362dad34458b6bfa4ba
Konstantin Bezgatsev <gelandewagen1@gmail.com>
2023-07-11 18:06:34 +0200

    ClientController

--- src/main/java/com/project/bankapp/controller/ClientController.java
+++ src/main/java/com/project/bankapp/controller/ClientController.java
@@ -5,10 +5,7 @@ import com.project.bankapp.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,10 +25,34 @@ public class ClientController {
     }
 
     @GetMapping(value = "/clients")
-    public ResponseEntity<List<Client>> read() {
+    public ResponseEntity<List<Client>> readAll() {
         final List<Client> clients = clientService.readAll();
         return clients != null && !clients.isEmpty()
                 ? new ResponseEntity<>(clients, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/clients/{id}")
+    public ResponseEntity<Client> read(@PathVariable(name = "uuid") int uuid) {
+        final Client client = clientService.read(uuid);
+        return client != null
+                ? new ResponseEntity<>(client, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(value = "/clients/{id}")
+    public ResponseEntity<?> update(@PathVariable(name = "uuid") int uuid, @RequestBody Client client) {
+        final boolean updated = clientService.update(client, uuid);
+        return updated
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+    @DeleteMapping(value = "/clients/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "uuid") int uuid) {
+        final boolean deleted = clientService.delete(uuid);
+        return deleted
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 }
